3aedaf3fea7f946986e835a506bf263c29d7a731
Samuel Sherlock <sherlock_s_2018@outlook.com>
2026-07-28 17:27:33 +1000

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,19 @@
+integer_type = 10
+
+float_type = 1.0
+
+bool_type = True
+
+string_type = 'him'
+
+list_type = []
+
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
