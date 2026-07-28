9d2de1fd5bc40c01a3a1bddc580f57c1c0752afa
Samuel Sherlock <sherlock_s_2018@outlook.com>
2026-07-28 17:29:59 +1000

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 1.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'string'
+print(type(variable))
+variable = []
+print(type(variable))
