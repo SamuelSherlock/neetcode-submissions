cdf5331f86656aac0005ced00dc1c752292f92c3
Samuel Sherlock <sherlock_s_2018@outlook.com>
2026-08-08 16:02:32 +1000

    Add: max-consecutive-ones - submission-2

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int count2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                count++;
+                count2 = max(count2, count);
+            } else {
+                    count = 0;
+            }
+        }
+        return count2;
+    }    
+};
